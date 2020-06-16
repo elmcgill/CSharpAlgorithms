cb8c4745507703b842f912953cfd0707127cae61
elmcgill <elmcgill@iastate.edu>
2020-06-16 15:31:06 -0600

    Finished the chapter on linear search

---
+++ LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 7;
+            int[] arr = new int[] { 1, 4, 5, 7, 9, 22 };
+
+            Console.Write("Our array contains: ");
+            Array.ForEach(arr, x => Console.Write(x + " "));
+
+            Console.WriteLine($"\nThe result of the search was index: {linearSearch(arr, val)}");
+        }
+
+        static int linearSearch(int[] a, int x)
+        {
+            int answer = -1;
+            for(int i=0; i<a.Length; i++)
+            {
+                if(a[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
